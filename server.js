c2f2077b9d30d82985fa82fc8875bb578f672654
marcelrusu <marcelrusu0@gmail.com>
2017-12-05 14:56:48 -0500

    remove useless retry logic, refactor out screenshot function

--- server.js
+++ server.js
@@ -10,27 +10,33 @@ const browserOptions = Object.freeze({
   ]
 })
 
-let retries = 0
+const browserPromise = puppeteer.launch(options).catch(console.log)
 
-let browser = puppeteer
-  .launch(browserOptions)
-  .catch(() => {
-    // Assuming the browser crashed
-    if (retries > 10)
-      throw 'Chrome crashed more then 10 times'
-    
-    retries++
-    if (browser)
-      browser.then(b => {
-        if (b) {
-          b.close()
-        }
-        
-        browser = puppeteer.launch(browserOptions)
-      })
-    else 
-      browser = puppeteer.launch(browserOptions)
-  })
+const screenshot = async (headers, browserPromise, url, width=800, height=600, x=0, y=0) => {
+  const browser = await browserPromise
+
+  let page
+  try { 
+    page = await browser.newPage()
+    await page.setViewport({ width, height })
+
+    await page.setExtraHTTPHeaders(headers)
+
+    await page.goto(url, { waitUntil: 'networkidle2' })
+
+    let options = { fullpage: true }
+
+    if (x && y && width && height)
+      options = {
+        clip: { x, y, width, height }
+      }
+
+    return await page.screenshot(options)    
+  } finally {
+    if (page)
+      await page.close()
+  }
+}
 
 app.get('/screenshot', (req, res) => {
   let { url, width, height, x, y } = req.query
@@ -42,56 +48,43 @@ app.get('/screenshot', (req, res) => {
   x = parseInt(x, 10)
   y = parseInt(y, 10)
 
-  browser.then(async (browser) => {
-    let page
-    try { 
-      page = await browser.newPage()
-      await page.setViewport({ width, height })
-      /**
-       * pass through cookies, auth, etc. 
-       * Using rawHeaders to ensure the values are strings
-       * `req.headers` could have array values 
-      */
-      const headers = req.rawHeaders.reduce((prev, cur, i, array) => {
-        if (i % 2 === 0)
-          return {...prev,
-            [cur]: array[i + 1]
-          }
-        else
-          return prev
-      }, [])
+  /**
+   * pass through cookies, auth, etc. 
+   * Using rawHeaders to ensure the values are strings
+   * `req.headers` could have array values 
+  */
+  const headers = req.rawHeaders.reduce((prev, cur, i, array) => {
+    if (i % 2 === 0)
+      return {...prev,
+        [cur]: array[i + 1]
+      }
+    else
+      return prev
+  }, [])
 
-      await page.setExtraHTTPHeaders(headers)
+  let picture
 
-      await page.goto(url, { waitUntil: 'networkidle2' })
-
-      let options = { fullpage: true }
-
-      if (x && y && width && height)
-        options = {
-          clip: { x, y, width, height }
-        }
-
-      return await page.screenshot(options)    
-    } finally {
-      if (page)
-        await page.close()
-    }
-  })
-  .catch(e => {
-    res.status(500)
-      .send(`Puppeteer Failed 
-        - url: ${url} 
-        - width: ${width} 
-        - height: ${height} 
-        - x: ${x} 
-        - y: ${y} 
-        - stacktrace: \n\n${e.stack}`)
-  })
-  .then(img => {
-    res.set('Content-type', 'image/png').send(img)
-  })
-    
+  if (width && height && x && y)
+    picture = screenshot(headers, browserPromise, url, width, height, x, y)
+  else if (width && height)
+    picture = screenshot(headers, browserPromise, url, width, height)
+  else if (x && y)
+    picture = screenshot(headers, browserPromise, url, 800, 600, x, y)
+  
+  picture
+    .catch(e => {
+      res.status(500)
+        .send(`Puppeteer Failed 
+          - url: ${url} 
+          - width: ${width} 
+          - height: ${height} 
+          - x: ${x} 
+          - y: ${y} 
+          - stacktrace: \n\n${e.stack}`)
+    })
+    .then(img => {
+      res.set('Content-type', 'image/png').send(img)
+    })
 })
 
 app.listen(port, () => console.log(`server listening on port ${port}`))
