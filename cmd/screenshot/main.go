76a95900ddfe830f8fabb1ebca630b72efca9dd0
marcelrusu <marcelrusu0@gmail.com>
2017-12-14 11:29:49 -0500

    add main.go

---
+++ cmd/screenshot/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/pressly/screenshot"
+)
+
+/*
+ * Example client usage
+ */
+
+func main() {
+	client := screenshot.New("http://localhost:3000")
+	window := new([2]int)
+	window[0] = 1000
+	window[1] = 1000
+
+	png, _ := client.PNG("https://golang.org/pkg/fmt/", screenshot.ScreenshotOptions{
+		Window: window,
+	})
+
+	pngB, _ := ioutil.ReadAll(png)
+
+	ioutil.WriteFile("./img.png", pngB, 0644)
+
+	p, _ := client.PDF("https://golang.org/pkg/fmt/", screenshot.PdfOptions{})
+
+	pdf, _ := ioutil.ReadAll(p)
+
+	ioutil.WriteFile("./doc.pdf", pdf, 0644)
+
+	j, _ := client.JPEG("https://golang.org/pkg/fmt/", screenshot.ScreenshotOptions{}, 100)
+
+	jpeg, _ := ioutil.ReadAll(j)
+
+	ioutil.WriteFile("./img.jpeg", jpeg, 0644)
+
+}
